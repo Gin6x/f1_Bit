a5373da461eb1078d9e3633400e5e8c376a6e7f7
Gene Chan <ccl0413gin@gmail.com>
2022-03-15 23:12:38 +0000

    added a readme file

---
+++ F1Bit/F1Bit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1Bit
+//
+//  Created by Gin on 15/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
