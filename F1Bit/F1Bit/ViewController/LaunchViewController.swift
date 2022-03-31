2c08b97270669a05a8a665f1c763829aed153ae8
Gene Chan <ccl0413gin@gmail.com>
2022-03-31 19:55:01 +0100

    Added pod 'DropdownMenu'

--- F1Bit/F1Bit/ViewController/LaunchViewController.swift
+++ F1Bit/F1Bit/ViewController/LaunchViewController.swift
@@ -11,7 +11,6 @@ class LaunchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("123")
     }
     
     @IBAction func  didTapButton() {
---
+++ F1Bit/F1Bit/ViewController/RankingPage/RankingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RankingViewController.swift
+//  F1Bit
+//
+//  Created by Gin on 31/3/2022.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController {
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var DriverBtn: UIButton!
+    @IBOutlet weak var TeamBtn: UIButton!
+    @IBOutlet weak var dropDownMenuView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
