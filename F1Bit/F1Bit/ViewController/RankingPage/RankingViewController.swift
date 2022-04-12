1a5cd2abca64ad05be849ab40cf2288dcd3f633b
Gene Chan <ccl0413gin@gmail.com>
2022-04-12 19:24:35 +0100

    initial rankingTableView setup added

--- F1Bit/F1Bit/ViewController/RankingPage/RankingViewController.swift
+++ F1Bit/F1Bit/ViewController/RankingPage/RankingViewController.swift
@@ -7,14 +7,29 @@
 
 import UIKit
 
-class RankingViewController: UIViewController {
+class RankingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var rankingButton: UIButton!
     @IBOutlet weak var seasonButton: UIButton!
+    @IBOutlet weak var rankingTableView: UITableView!
+    
+    //Data model for cell display
+    let f1Teams = ["Mercedes-AMG Petronas", "Red Bull Racing", "Scuderia Ferrari", "McLaren Racing", "Alpine F1 Team"]
+    
+    // cell reuse id
+    let cellReuseIdentifier = "teamCell"
+    let cellSpacingHeight: CGFloat = 5
 
     override func viewDidLoad() {
         super.viewDidLoad()
         initialF1ApiCall()
+        
+        //Register the table view cell class and its reuse id
+        self.rankingTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        rankingTableView.delegate = self
+        rankingTableView.dataSource = self
+        rankingTableView.layer.cornerRadius = 10
         //        getOptions()
     }
 //    func getOptions() {
@@ -24,6 +39,50 @@ class RankingViewController: UIViewController {
 //
 //        self.driverBtn.setOptions(options: options, optionIds: ids, selectedIndex: 0)
 //    }
+    //=================================================rankingTableView================================================================//
+    
+    // number of section for each cell
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return self.f1Teams.count
+        }
+    
+    // number of rows in table view
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+    
+    // set the spacing between sections
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            return cellSpacingHeight
+        }
+    
+    // Make the background color show through
+        func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+            let headerView = UIView()
+            headerView.backgroundColor = UIColor.clear
+            return headerView
+        }
+    
+    // create a cell for each table view row
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            // create a new cell if needed or reuse an old one
+            let teamCell:UITableViewCell = (self.rankingTableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+            
+            // set the text from the data model
+            teamCell.textLabel?.text = self.f1Teams[indexPath.section]
+            
+            // add border and color
+            teamCell.backgroundColor = UIColor.white
+            teamCell.layer.borderColor = UIColor.black.cgColor
+            teamCell.layer.borderWidth = 1
+            teamCell.layer.cornerRadius = 8
+            teamCell.clipsToBounds = true
+
+            return teamCell
+        }
+    
+    //=====================================================rankingTableView==================================================================//
     
     @IBAction func rankingPickerButton(_ sender: UIButton) {
         let selectionPickerVC = storyboard?.instantiateViewController(withIdentifier: "SelectionPicker_VC") as! SelectionPickerViewController
